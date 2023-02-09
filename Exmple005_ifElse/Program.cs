fc335fef51dbc02240397e48ec295e697b944e93
Davyd.AR <9766288@mail.ru>
2023-02-09 22:02:25 +0300

    Exmple005_ifElse

---
+++ Exmple005_ifElse/Program.cs
@@ -0,0 +1,15 @@
+﻿// Условие на определение особенного имени
+string username;
+Console.Write("Введите имя пользователя: ");
+username = Console.ReadLine();
+
+if (username.ToLower() == "alex")
+{
+    Console.WriteLine("Здравствуй, создатель!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.Write("!");
+}
